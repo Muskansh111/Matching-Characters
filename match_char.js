e7446f8e18161d76b62b415d9ea46479a2efe421
Muskansh111 <111625853+Muskansh111@users.noreply.github.com>
2022-08-20 11:21:54 +0530

    Create match_char.js

---
+++ match_char.js
@@ -0,0 +1,17 @@
+(function MatchingCharacters(str) { 
+    var count  = 0
+    for(var i = 0; i < str.length; i++){
+      var charObj = {}
+      var lastIdx = str.lastIndexOf(str[i])
+      if(i == lastIdx) continue
+      for(var j = i+1; j < lastIdx; j++){
+        if(charObj[str[j]] == undefined) charObj[str[j]] = str[j]
+      }
+      if(count < Object.keys(charObj).length) count = Object.keys(charObj).length
+    }
+    //console.log(count)
+    return count
+})
+
+//function calling 
+console.log(MatchingCharacters(readline()));      //Required input is --->  "mmmerme"
